bf26552a895a9f1d23106ab477a4111164ef38f6
Maciej Urban <maciej-urban@outlook.com>
2025-01-30 16:53:22 +0100

    fix: bad import

--- src/lib.zig
+++ src/lib.zig
@@ -13,7 +13,6 @@ pub const JPGRecoverer = filetypes.JPGRecoverer;
 pub const PNGRecoverer = filetypes.PNGRecoverer;
 
 pub const FilesystemHandler = filesystems.FilesystemHandler;
-pub const FsType = filesystems.FsType;
 
 test {
     std.testing.refAllDecls(@This());
