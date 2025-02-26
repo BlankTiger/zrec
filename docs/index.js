3f87edf15e609b2107a7bec4c594aaae4c943ece
kcov <kcov@test.com>
2025-02-26 20:00:28 +0000

    coverage report

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"test.320b330d/index.html","title":"test","summary_name":"test","covered_class":"lineCov","covered":"87.4","covered_lines":"1029","uncovered_lines":"148","total_lines" : "1177"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "test", "date" : "2025-02-26 20:00:27", "instrumented" : 1177, "covered" : 1029,};
---
+++ docs/test.320b330d/index.js
@@ -0,0 +1,17 @@
+var data = {files:[
+{"link":"testing_utils.zig.fc29535.html","title":"testing_utils.zig","summary_name":"[...]/zrec/src/filesystems/testing_utils.zig","covered_class":"lineCov","covered":"89.7","covered_lines":"35","uncovered_lines":"4","total_lines" : "39"},
+{"link":"testing_utils.zig.e17c5c28.html","title":"testing_utils.zig","summary_name":"[...]/zrec/src/filetypes/testing_utils.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"18","uncovered_lines":"0","total_lines" : "18"},
+{"link":"NTFS.zig.402b24be.html","title":"NTFS.zig","summary_name":"[...]/zrec/src/filesystems/NTFS.zig","covered_class":"linePartCov","covered":"33.3","covered_lines":"2","uncovered_lines":"4","total_lines" : "6"},
+{"link":"EXT2.zig.30adaff1.html","title":"EXT2.zig","summary_name":"[...]/zrec/src/filesystems/EXT2.zig","covered_class":"lineCov","covered":"78.7","covered_lines":"381","uncovered_lines":"103","total_lines" : "484"},
+{"link":"FAT.zig.f52623be.html","title":"FAT.zig","summary_name":"[...]/zrec/src/filesystems/FAT.zig","covered_class":"lineCov","covered":"95.3","covered_lines":"82","uncovered_lines":"4","total_lines" : "86"},
+{"link":"utils.zig.8b25f60e.html","title":"utils.zig","summary_name":"[...]/zrec/src/utils.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"2","uncovered_lines":"0","total_lines" : "2"},
+{"link":"printer.zig.5074c357.html","title":"printer.zig","summary_name":"[...]/zrec/src/printer.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"49","uncovered_lines":"0","total_lines" : "49"},
+{"link":"png.zig.47b942df.html","title":"png.zig","summary_name":"[...]/zrec/src/filetypes/png.zig","covered_class":"lineCov","covered":"88.2","covered_lines":"120","uncovered_lines":"16","total_lines" : "136"},
+{"link":"jpg.zig.bdcbe2b4.html","title":"jpg.zig","summary_name":"[...]/zrec/src/filetypes/jpg.zig","covered_class":"lineCov","covered":"94.7","covered_lines":"126","uncovered_lines":"7","total_lines" : "133"},
+{"link":"reader.zig.b36a6000.html","title":"reader.zig","summary_name":"[...]/zrec/src/reader.zig","covered_class":"lineCov","covered":"96.2","covered_lines":"151","uncovered_lines":"6","total_lines" : "157"},
+{"link":"FilesystemHandler.zig.b600a96e.html","title":"FilesystemHandler.zig","summary_name":"[...]/zrec/src/FilesystemHandler.zig","covered_class":"lineCov","covered":"93.8","covered_lines":"60","uncovered_lines":"4","total_lines" : "64"},
+{"link":"lib.zig.97e908ad.html","title":"lib.zig","summary_name":"[...]/zrec/src/lib.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"2","uncovered_lines":"0","total_lines" : "2"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "test", "date" : "2025-02-26 20:00:27", "instrumented" : 1176, "covered" : 1028,};
+var merged_data = [];
