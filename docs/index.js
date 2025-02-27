09ac065d69a5773f311149be6df840d02c1d3219
kcov <kcov@test.com>
2025-02-27 20:56:13 +0000

    coverage report

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"test.320b330d/index.html","title":"test","summary_name":"test","covered_class":"lineCov","covered":"87.4","covered_lines":"1029","uncovered_lines":"148","total_lines" : "1177"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "test", "date" : "2025-02-27 20:56:12", "instrumented" : 1177, "covered" : 1029,};
